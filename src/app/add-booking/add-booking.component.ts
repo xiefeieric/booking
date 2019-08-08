b3087db6954ba91c90271e15995d1751cbdda7b5
Fei <xiefeieric@gmail.com>
2019-08-08 10:49:19 +0100

    refactor

--- src/app/add-booking/add-booking.component.ts
+++ src/app/add-booking/add-booking.component.ts
@@ -1,13 +1,9 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import * as moment from 'moment';
-import {DateUtil} from '../util/date-util';
-import {WorkdayFormatEnum} from '../enums/workday-format.enum';
 import {ProcessDatesModel} from '../model/process-dates.model';
 import {ClrModal} from '@clr/angular';
-import {AngularFirestore} from '@angular/fire/firestore';
 import {BookingModel} from '../model/booking.model';
-import {Router} from '@angular/router';
-import {AngularFireFunctions} from '@angular/fire/functions';
+import {BookingService} from '../services/booking.service';
 
 @Component({
   selector: 'app-add-booking',
@@ -25,13 +21,11 @@ export class AddBookingComponent implements OnInit {
   public patientName = 'David Clayton';
   public processingDays: ProcessDatesModel;
 
-  constructor(public db: AngularFirestore,
-              public fns: AngularFireFunctions,
-              public router: Router) {
+  constructor(public bookingService: BookingService) {
   }
 
   ngOnInit() {
-    this.minTreatDay = DateUtil.addWorkdays(moment().add(7, 'weeks').toDate(), 1, WorkdayFormatEnum.FIVE);
+    this.minTreatDay = this.bookingService.getMinAvailableTreatDay();
   }
 
   public dateChange(date: Date) {
@@ -41,48 +35,11 @@ export class AddBookingComponent implements OnInit {
       return;
     }
 
-    this.getProcessingDays(date)
+    this.bookingService.getProcessingDays(date)
       .then((value: string) => {
         this.processingDays = JSON.parse(value).processingDays as ProcessDatesModel;
-        this.processList = this.addProcessDaysToList(this.processingDays);
+        this.processList = this.bookingService.addProcessDaysToList(this.processingDays);
       });
-  }
-
-  private async getProcessingDays(date: Date): Promise<string> {
-    const callable = this.fns.httpsCallable('getProcessingDays');
-    try {
-      return await callable(JSON.stringify(date)).toPromise();
-    } catch (e) {
-      console.error('Firebase function failed!', e);
-    }
-
-    // let datePointer = date;
-    // const processingDaysModel = new ProcessDatesModel();
-    // processingDaysModel.trasnport4 = DateUtil.addWorkdays(datePointer, -1, WorkdayFormatEnum.FOUR);
-    // datePointer = processingDaysModel.trasnport4;
-    // processingDaysModel.manufacturingEnd = DateUtil.addWorkdays(datePointer, -1, WorkdayFormatEnum.FOUR);
-    // datePointer = processingDaysModel.manufacturingEnd;
-    // processingDaysModel.manufacturingStart = DateUtil.addWorkdays(datePointer, -6, WorkdayFormatEnum.FOUR);
-    // datePointer = processingDaysModel.manufacturingStart;
-    // processingDaysModel.trasnport3 = DateUtil.addWorkdays(datePointer, -1, WorkdayFormatEnum.FOUR);
-    // datePointer = processingDaysModel.trasnport3;
-    // processingDaysModel.specimenProcessEnd = DateUtil.addWorkdays(datePointer, -1, WorkdayFormatEnum.FOUR);
-    // datePointer = processingDaysModel.specimenProcessEnd;
-    // processingDaysModel.specimenProcessStart = DateUtil.addWorkdays(datePointer, -14, WorkdayFormatEnum.FOUR);
-    // datePointer = processingDaysModel.specimenProcessStart;
-    // processingDaysModel.trasnport2 = DateUtil.addWorkdays(datePointer, -1, WorkdayFormatEnum.FOUR);
-    // datePointer = processingDaysModel.trasnport2;
-    // processingDaysModel.surgeryEnd = DateUtil.addWorkdays(datePointer, -1, WorkdayFormatEnum.FOUR);
-    // processingDaysModel.surgeryStart = processingDaysModel.surgeryEnd;
-    // datePointer = processingDaysModel.surgeryStart;
-    // processingDaysModel.trasnport1 = DateUtil.addWorkdays(datePointer, -1, WorkdayFormatEnum.FOUR);
-    // datePointer = processingDaysModel.trasnport1;
-    // processingDaysModel.deliverCollectionKitEnd = DateUtil.addWorkdays(datePointer, -1, WorkdayFormatEnum.FOUR);
-    // processingDaysModel.deliverCollectionKitStart = processingDaysModel.deliverCollectionKitEnd;
-    // processingDaysModel.treatDayStart = date;
-    // processingDaysModel.treatDayEnd = date;
-    //
-    // return processingDaysModel;
   }
 
   public addBooking() {
@@ -100,44 +57,11 @@ export class AddBookingComponent implements OnInit {
     const timeStamp = moment().valueOf();
     booking.createdAt = timeStamp;
     booking.updatedAt = timeStamp;
-    await this.db.collection('bookings').add(JSON.parse(JSON.stringify(booking)));
-    await this.router.navigateByUrl('booking');
-  }
-
-  private addProcessDaysToList(processingDays: ProcessDatesModel) {
-    const deliverCollectionKit = {
-      title: 'Deliver Collection Kit',
-      content: moment(this.parseDate(processingDays.deliverCollectionKitStart)).format('DD/MMM/YYYY').toString()
-    };
-    const surge = {
-      title: 'Surgery/Apheresis',
-      content: moment(this.parseDate(processingDays.surgeryStart)).format('DD/MMM/YYYY').toString()
-    };
-    const specimenProcessing = {
-      title: 'Specimen Processing',
-      content: `${moment(this.parseDate(processingDays.specimenProcessStart)).format('DD/MMM/YYYY').toString()} - ${moment(this.parseDate(processingDays.specimenProcessEnd)).format('DD/MMM/YYYY').toString()}`
-    };
-    const manufacturing = {
-      title: 'Manufacturing',
-      content: `${moment(this.parseDate(processingDays.manufacturingStart)).format('DD/MMM/YYYY').toString()} - ${moment(this.parseDate(processingDays.manufacturingEnd)).format('DD/MMM/YYYY').toString()}`
-    };
-    const treat = {
-      title: 'Infusion/Treatment Day',
-      content: moment(this.parseDate(processingDays.treatDayStart)).format('DD/MMM/YYYY').toString()
-    };
-
-    return [deliverCollectionKit, surge, specimenProcessing, manufacturing, treat];
-  }
-
-  private parseDate(str: string): Date {
-    if (!str) {
-      return;
-    }
-    return new Date(str);
+    await this.bookingService.saveBooking(booking);
   }
 
   public dateFilter = (d: Date): boolean => {
     const day = d.getDay();
     return day !== 0 && day !== 6;
-  }
+  };
 }
